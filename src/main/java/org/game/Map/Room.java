c5ba40503f2f6907a9ee8b2859d6d8ea7d79a64e
Kuruczattila2003 <kuruczattila2b@gmail.com>
2024-03-18 08:47:13 +0100

    room refactored

--- src/main/java/org/game/Map/Room.java
+++ src/main/java/org/game/Map/Room.java
@@ -1,31 +1,55 @@
 package main.java.org.game.Map;
 
+import main.java.org.game.Isten;
+import main.java.org.game.physics.Collider;
+import main.java.org.game.physics.ColliderGroup;
+import main.java.org.game.updatable.Updatable;
+
 import java.util.ArrayList;
 
-public class Room {
-    private int ID;
+public class Room extends Updatable {
+    int ID;
     private ArrayList<UnitRoom> unitRooms;
     private ArrayList<Room> adjacentRooms;
-    private int playerCount;
+    int playerCount;
     //ArrayList<Player> players;
-    private boolean discovered;
-    private RoomType roomType;
+    boolean discovered;
+    RoomType roomType;
+    private ArrayList<Integer> hasDoorWith;
+
+    ColliderGroup roomColliders;
+
     public Room(int ID){
         this.ID = ID;
         unitRooms = new ArrayList<>();
         adjacentRooms = new ArrayList<>();
+        roomColliders=new ColliderGroup();
+        hasDoorWith = new ArrayList<>();
     }
-    public boolean isAdjacent(Room room) {
-       // if(this.equals(room)) return false; egynelore nem kell lekezelni
+
+    @Override
+    public void onStart(Isten isten) {
+        isten.getPhysicsEngine().addColliderGroup(roomColliders);
+    }
+
+    @Override
+    public void onUpdate(Isten isten, double deltaTime) {
+
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    boolean isAdjacent(Room room) {
+        // if(this.equals(room)) return false; egynelore nem kell lekezelni
         for(Room checkRoom : adjacentRooms) {
             if (room.equals(checkRoom)) return true;
-            //if(room.getID() == checkRoom.getID()) return true;
         }
         return  false;
     }
 
-
-    //getters and setters
     public ArrayList<UnitRoom> getUnitRooms() {
         return unitRooms;
     }
@@ -37,30 +61,17 @@ public class Room {
         return ID;
     }
 
-    public void setID(int ID) {
-        this.ID = ID;
+    public void addCollider(Collider c){roomColliders.addCollider(c);}
+
+    public void addHasDoorWith(int i) {
+        hasDoorWith.add(i);
     }
 
-    public void setPlayerCount(int playerCount) {
-        this.playerCount = playerCount;
+    public boolean hasDoorWith(int i) {
+        return hasDoorWith.contains(i);
     }
 
-    public void setDiscovered(boolean discovered) {
-        this.discovered = discovered;
-    }
-    public boolean isDiscovered(){
-        return discovered;
-    }
-
-    public void setRoomType(RoomType roomType) {
-        this.roomType = roomType;
-    }
-
-    public int getPlayerCount() {
-        return playerCount;
-    }
-
-    public RoomType getRoomType() {
-        return roomType;
+    public boolean hasDoor() {
+        return (!hasDoorWith.isEmpty());
     }
 }
