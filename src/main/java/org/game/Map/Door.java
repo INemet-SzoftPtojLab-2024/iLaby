3ef35ece6207b6f30a827fe67dd62d327f6ae4bb
László Áron Péter <laszloaron03@gmail.com>
2024-04-13 12:48:30 +0200

    updating this branch

--- src/main/java/org/game/Map/Door.java
+++ src/main/java/org/game/Map/Door.java
@@ -2,7 +2,8 @@ package main.java.org.game.Map;
 
 import main.java.org.game.Graphics.Image;
 import main.java.org.linalg.Vec2;
-
+//TODO abstrakt os az ajtonak meg a wallnak
+//az edgemanagerben a updateaftermergeben van egy door castolas, azt ki kell venni ha megvan az abstrakt os!!!!!!!!
 public class Door extends Wall {
     public Door(Image i){ //odl!!  do not use!!!
         super(null, i);
