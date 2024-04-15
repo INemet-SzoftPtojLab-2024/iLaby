135e907712e4ee540c91d9f8a028e8332afbb2f7
BlaskovitsBence <bence.blaskovits@gmail.com>
2024-04-15 14:46:01 +0200

    added lost classes again

---
+++ src/main/java/org/items/Item.java
@@ -0,0 +1,63 @@
+package main.java.org.items;
+
+import main.java.org.game.Graphics.Image;
+import main.java.org.game.Graphics.ImageUI;
+import main.java.org.game.Isten;
+import main.java.org.linalg.Vec2;
+
+import java.time.LocalDateTime;
+
+/*TVSZ denevérbőrre nyomtatott példányai
+Gasmask
+Transistor
+Logarléc
+Söröspohár, rongy (adott ideig véd a boss-tól)
+Camambert (gázszobát csinál, bénítja a boss-t)*/
+public abstract class Item {
+    public enum Location {
+        CHEST,
+        GROUND,
+        INVENTORY
+    };
+    protected final Vec2 scale;
+    protected Location location;
+    protected Vec2 position;
+    protected Image image;
+    protected final Isten isten;
+    protected String imagePath;
+
+    private LocalDateTime droppedAt;
+    public Item(Isten isten,Vec2 scale){
+        this.scale=scale;
+        location=Location.CHEST;
+        position=null;
+        droppedAt = null;
+        this.isten=isten;
+        isten.getItemManager().addItem(this);
+    }
+    public void dropOnGround(Vec2 pos){
+        if(!location.equals(Location.GROUND)){
+            location = Location.GROUND;
+            position = pos;
+            image.setPosition(pos);
+            image.setVisibility(true);
+            droppedAt = LocalDateTime.now();
+        }
+    }
+    public void pickUpInInventory(){
+        //Pics up an item if it is not in the inventory, and it has been dropped for more than 200 millisec
+        //1 ms = 1000000 ns :)
+        if((!location.equals(Location.INVENTORY) && droppedAt.isBefore((LocalDateTime.now()).minusNanos(200000000)))) {
+            location = Location.INVENTORY;
+            image.setVisibility(false);
+            isten.getInventory().addItem(this);
+        }
+    }
+
+    public void use(){
+
+    }
+    public String getImagePath(){return imagePath;}
+    public Vec2 getPosition(){return position;}
+    public Location getLocation(){return location;}
+}
---
+++ src/main/java/org/items/usable_items/Transistor.java
@@ -0,0 +1,69 @@
+package main.java.org.items.usable_items;
+
+import main.java.org.game.Graphics.Image;
+import main.java.org.game.Graphics.Renderable;
+import main.java.org.game.Graphics.TextUI;
+import main.java.org.game.Isten;
+import main.java.org.game.UI.Inventory;
+import main.java.org.items.Item;
+import main.java.org.linalg.Vec2;
+
+public class Transistor extends Item {
+
+    private TextUI countText;
+    private Image activatedImage;
+
+    boolean used = false;
+    public Transistor(Isten isten){
+        super(isten,new Vec2(0.4f,0.5f));
+        imagePath="./assets/items/item_transistor.png";
+        image = new Image(new Vec2(-10,-10), scale, imagePath);
+        activatedImage = new Image(new Vec2(-10,-10), scale, "./assets/items/item_transistor_activated.png");
+        isten.getRenderer().addRenderable(image);
+        isten.getRenderer().addRenderable(activatedImage);
+        image.setVisibility(false);
+        activatedImage.setVisibility(false);
+
+        countText = new TextUI("2", new Vec2(0,0), "./assets/Monocraft.ttf", 15, 255, 255, 255);
+        countText.setVisibility(false);
+        countText.setAlignment(Renderable.CENTER, Renderable.BOTTOM);
+        countText.setSortingLayer(-80);
+        countText.setShadowOn(false);
+        isten.getRenderer().addRenderable(countText);
+
+    }
+
+    @Override
+    public void pickUpInInventory(){
+        super.pickUpInInventory();
+        Inventory inv = isten.getInventory();
+        Vec2 slotPosition = inv.getStoringSlotPosition(this);
+        Vec2 textPosition = new Vec2(slotPosition.x -10, slotPosition.y-7);
+        if(!used){
+            countText.setPosition(textPosition);
+            countText.setVisibility(true);
+        }
+
+    }
+
+    @Override
+    public void use(){
+        if(!used){
+            countText.setVisibility(false);
+            Vec2 playerPosition = isten.getPlayer().getPlayerCollider().getPosition();
+            activatedImage.setPosition(playerPosition);
+            activatedImage.setVisibility(true);
+            used=true;
+        }
+        else{
+            isten.getPlayer().getPlayerCollider().setPosition(activatedImage.getPosition());
+        }
+
+    }
+    @Override
+    public void dropOnGround(Vec2 pos){
+        super.dropOnGround(pos);
+        countText.setVisibility(false);
+    }
+
+}
