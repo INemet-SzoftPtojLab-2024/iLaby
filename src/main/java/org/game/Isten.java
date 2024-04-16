918560770727fde8774d46e654fcd86e5e2495fb
Bükkösi Beatrix <beatrix.bukkosi@gmail.com>
2024-04-16 17:09:09 +0200

    Improvements to villain movement

--- src/main/java/org/game/Isten.java
+++ src/main/java/org/game/Isten.java
@@ -119,9 +119,9 @@ public class Isten {
         updatables.add(map);
         updatables.add(new ChestManager(75));//majd a játékba nem kell 500 láda, csak szemléltetésképp kell ilyen sok
 
-        updatables.add(new Villain("Gajdos", new Vec2(8,7), "./assets/villain/villain1.png"));
-        updatables.add(new Villain("Csuka", new Vec2(5,5), "./assets/villain/villain2.png"));
-        updatables.add(new Villain("Villain 3", new Vec2(3,3), "./assets/villain/villain3.png"));
+        updatables.add(new Villain("Gajdos",  "./assets/villain/villain1.png"));
+        updatables.add(new Villain("Csuka",  "./assets/villain/villain2.png"));
+        updatables.add(new Villain("Villain 3",  "./assets/villain/villain3.png"));
         updatables.add(new TimeCounter());
         updatables.add(new Help());
         updatables.add(new GameMenu());
