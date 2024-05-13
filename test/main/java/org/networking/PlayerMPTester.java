d1ae1b09f2fbb420d0428759954ef16c45a1610f
dudasviktor <dudas.viktor22@gmail.com>
2024-05-13 17:58:57 +0200

    playermptests++

--- test/main/java/org/networking/PlayerMPTester.java
+++ test/main/java/org/networking/PlayerMPTester.java
@@ -1,6 +1,8 @@
 package main.java.org.networking;
 
 import main.java.org.game.Isten;
+import main.java.org.game.physics.Collider;
+import main.java.org.linalg.Vec2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.net.InetAddress;
@@ -9,12 +11,14 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PlayerMPTester {
 
     private Isten isten;
-    private PlayerMP player;
+    private PlayerMP player, player2;
 
     @BeforeEach
     public void setUp() {
         isten = new Isten();
         player = new PlayerMP(isten, "TestPlayer", InetAddress.getLoopbackAddress(), 12345);
+        player2 = new PlayerMP(isten, "TestPlayer", InetAddress.getLoopbackAddress(), 12345, new Vec2(0,0));
+        player.setPlayerCollider(new Collider(new Vec2(0,0),new Vec2(0.5f, 0.5f)));
     }
 
     @Test
